9ef71e9d86633ccedfa357d8257f280279131ba3
Devlepa <juanestebanleonparra@outlook.es>
2024-09-22 13:13:13 -0500

    ajuste frontend y backend y su conexion efectivamente se reciben los datos del formulario y esta delimitado

--- my-backend/server.js
+++ my-backend/server.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2/promise');
-const userRoutes = require('./routes/userRoutes');
-const connection = require('./config/db'); // Assuming your db.js file is in the same directory
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3001; // Puedes cambiar el puerto si lo deseas
+const port = 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../my-frontend/public")));
 
-/* async function insertarUsuario(nombre, edad) {
-    try {
-      const [result] = await pool.execute('INSERT INTO users (username, edad) VALUES (?, ?)', [nombre, edad]);
-      console.log('Nuevo usuario insertado con ID:', result.insertId);
-    } catch (error) {
-      console.error('Error al insertar el usuario:', error);
-    }
-  }
-  
-insertarUsuario('Juan Perez', 35); */
-
-// Importar las rutas
-app.use(express.static(path.join(__dirname, '../my-frontend/public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../my-frontend/public', 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../my-frontend/public", "index.html"));
 });
 
+app.post('/submit', (req, res) => {
+    const cantidadEquipo = req.body.cantidadEquipo;
+    const equiposSonido = req.body.equiposSonido;
+    const valorAlquiler = req.body.valorAlquiler;
+    const requiereDecoracion = req.body.requiereDecoracion;
+    const requiereSeguridad = req.body.requiereSeguridad;
+    const requiereTransporte = req.body.requiereTransporte;
+    const requiereCocina = req.body.requiereCocina;
+
+    console.log(`Cantidad de equipo: ${cantidadEquipo}`);
+    console.log(`Equipos de sonido seleccionados: ${Array.isArray(equiposSonido) ? equiposSonido.join(', ') : equiposSonido}`);
+    console.log(`Valor de alquiler: ${valorAlquiler}`);
+    console.log(`Requiere decoración: ${requiereDecoracion}`);
+    console.log(`Requiere Seguridad: ${requiereSeguridad}`);
+    console.log(`Requiere Transporte: ${requiereTransporte}`);
+    console.log(`Requiere Cocina: ${requiereCocina}`);
+
+    res.send('Formulario recibido y datos procesados');
+});
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
